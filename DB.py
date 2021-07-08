5e54441456bb9d9d15ea0216908bf205c53c1c1f
shaked arbili <shaked.arbili@live.biu.ac.il>
2021-07-08 15:03:08 +0300

    load dataframe method was created using sqlalchemy

--- DB.py
+++ DB.py
@@ -1,4 +1,5 @@
 import pymysql
+from sqlalchemy import create_engine
 
 class DBHelper:
     def __init__(self):
@@ -6,6 +7,9 @@ class DBHelper:
         self.user = "root"
         self.password = "root"
         self.db = "terror_db"
+        self.con_string = 'mysql+mysqldb://' + self.user + ':' + self.password + '@' + self.host + ':3306/' \
+                  + self.db + '?charset=utf8mb4'
+        self.engine = create_engine(self.con_string)
 
     def __connect__(self):
         self.con = pymysql.connect(host=self.host, user=self.user, password=self.password, db=self.db, cursorclass=pymysql.cursors.
@@ -26,3 +30,7 @@ class DBHelper:
         self.__connect__()
         self.cur.execute(query)
         self.__disconnect__()
+
+    def load_df(self,df, df_name):
+        df.to_sql(name=df_name, con=self.engine, if_exists='replace', index=False, chunksize=1000,
+                           method='multi')
